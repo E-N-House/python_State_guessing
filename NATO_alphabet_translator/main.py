d7443454d2108e26d450d2c77bc03e504d3e5609
enhouse <elizabeth.house88@gmail.com>
2023-09-03 15:33:35 -0400

    restored input prompt

--- NATO_alphabet_translator/main.py
+++ NATO_alphabet_translator/main.py
@@ -6,16 +6,13 @@ nato_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 # use Dictionary comprehension to translate to letter:code format
 # {"A": "Alfa", "B": "Bravo"}
 nato_dict = {row.letter: row.code for (index, row) in nato_data_frame.iterrows()}
-print(nato_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 # prompt user input
-# TEMP BELOW
-user_input = "z a b"
-    # input("Enter the word you'd like to translate\n"))
+user_input = input("Enter the word you'd like to translate\n")
 
-# convert user input to to an upper case list for each letter
+# convert user input to an upper case list for each letter
 user_input_list = [letter.upper() for letter in user_input]
 
 # need to pull the code and input it into the phrase
@@ -34,6 +31,7 @@ user_phrase = ""
 #             user_phrase += f" {nato_dict[dict_letter]}"
 # print(user_phrase)
 
+# refactored above into list comprehension
 user_phrase_list_comp = [nato_dict[letter] for letter in user_input_list if letter in nato_dict]
 print(str.join("", user_phrase_list_comp))
 
