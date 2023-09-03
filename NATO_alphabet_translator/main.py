22d53786422a369f6d7b312af286e6776a274fe1
enhouse <elizabeth.house88@gmail.com>
2023-09-02 22:25:55 -0400

    nato_dict is in letter:code pairs created from csv file

--- NATO_alphabet_translator/main.py
+++ NATO_alphabet_translator/main.py
@@ -20,8 +20,13 @@ for (index, row) in student_data_frame.iterrows():
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+# nato_phonetic_alphabet.csv dataframe
+nato_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
+# use Dictionary comprehension to translate to letter:code format
+# {"A": "Alfa", "B": "Bravo"}
+nato_dict = {row.letter: row.code for (index, row) in nato_data_frame.iterrows()}
+
+
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
