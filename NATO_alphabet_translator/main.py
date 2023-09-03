6116c73a7aab467fa717a92e1a5fc910dd4691e7
enhouse <elizabeth.house88@gmail.com>
2023-09-03 16:38:43 -0400

    moved obsolete code to bottom of file. Refactored to code_phrase

--- NATO_alphabet_translator/main.py
+++ NATO_alphabet_translator/main.py
@@ -9,19 +9,24 @@ nato_dict = {row.letter: row.code for (index, row) in nato_data_frame.iterrows()
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-# prompt user input
-user_input = input("Enter the word you'd like to translate\n").upper()
-
+# prompt user input AND
 # convert user input to an upper case list for each letter
-user_input_list = [letter for letter in user_input]
+
+user_input = list(input("Enter the word you'd like to translate\n").upper())
 
 # need to pull the code and input it into the phrase
 
+# refactored above into list comprehension
+code_phrase = [f"{nato_dict[letter]} " for letter in user_input if letter in nato_dict]
+print(str.join("", code_phrase))
+
+
+# OLDER CODE
 # currently this is going through the nato alphabet and copying over the code if the letter is in the user input
-user_phrase_codes_present_list = [code for (letter, code) in nato_dict.items() if letter in user_input_list]
+# user_phrase_codes_present_list = [code for (letter, code) in nato_dict.items() if letter in user_input]
 
 # using for loop need to refactor into a list comprehension
-user_phrase = ""
+# user_phrase = ""
 # need to go through the letters in the user input and replace each with the corresponding nato code from the nato_dict
 # for letter in user_input_list:
 #     if letter == " ":
@@ -31,7 +36,4 @@ user_phrase = ""
 #             user_phrase += f" {nato_dict[dict_letter]}"
 # print(user_phrase)
 
-# refactored above into list comprehension
-user_phrase_list_comp = [f"{nato_dict[letter]} " for letter in user_input_list if letter in nato_dict]
-print(str.join("", user_phrase_list_comp))
 
